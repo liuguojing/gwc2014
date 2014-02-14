bba0ed4aea7163e24a688c2bfd62ea88618e0b66
leftarrow <cranelee@gmail.com>
2014-02-14 11:10:44 +0000

    Tony update chase text

--- protected/views/user/chase_email.php
+++ protected/views/user/chase_email.php
@@ -7,7 +7,7 @@
 <img src="<?php echo $this->domain?>img/email_head.png" />
 <p><b>Dear <?php echo $model->first_name;?>,</b></p>
 
-<p style="word-wrap:break-word;font-family:arial;">We have noticed that you still have not confirmed your acceptance for this year's Winners Circle.  The deadline to complete your registration for Winners Circle is <b>Friday February 21, 2014</b>.  It is essential you Accept or Decline by Friday so that we can secure your travel arrangements.</p>
+<p style="word-wrap:break-word;font-family:arial;">We have noticed that you still have not confirmed your acceptance for this year's Winners Circle.  The deadline to complete your registration for Winners Circle is <b>Friday February 21, 2014</b>.  It is essential you Accept or Decline by Friday so that we can secure your travel arrangements and you can book your tours.</p>
 
 <p style="word-wrap:break-word;font-family:arial;">If you need any assistance in completing the process, or need your invitation resent, please do let us know by contacting <a href="mailto:winners@corporate-reg.com">winners@corporate-reg.com</a>.</p>
 
